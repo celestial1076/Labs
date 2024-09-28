6057931e4cfed5d3b629422efb167eabcd3a68c9
twink <kluevdanil.ru1@gmail.com>
2024-09-28 17:36:34 +0300

    finish lab1

--- Lab1.java
+++ Lab1.java
@@ -1,13 +1,25 @@
+// Антошенко Д.Е. №1 вариант
+// Лаба №1
+// Финальный варик
+// Импорт сканера (ввод с консоли)
+import java.util.Scanner;
+
 public class Lab1 {
-    static int factorial(int n) {
-        int res = 1, i;
-        for (i = 2; i <= n; i++)
-                res *= i;
-        return res;
-    }
-    public static void main(String[] args)
-    {
-        int num = 5;
-        System.out.println("Factorial of " + num + " is " + factorial(5));
+// Расчет факториала
+    public static int CalculateFactorial(int n) {
+        int factorial = 1, i = 2;
+        while (i <= n) { // Условие
+            factorial *= i;
+            i++; // Увеличиваем на единицу множитель
         }
+        return factorial;
+    }
+// Делаем возможность вводить искомое число
+    public static void  main(String[] args) {
+        int number;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        number = scan.nextInt();
+        System.out.println("Факториал числа " + number + " равен: " + CalculateFactorial(number));
+    }
 }
